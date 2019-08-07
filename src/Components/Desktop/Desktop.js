676650ca05c804cc89c6780ebe2177c217305eeb
Kedar Bhanegaonkar <ksbhanegaonkar@gmail.com>
2019-08-07 09:11:47 +0530

    Added test code to Desktop Component

---
+++ src/Components/Desktop/Desktop.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class Desktop extends Component{
+
+    render() {
+        return (<div>
+          test
+        </div>)
+      }
+
+}
+export default Desktop;
